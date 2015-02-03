6fddbda8fb171c61c952c93afdcd2c72e5043d06
nateyb93 <brownn15@up.edu>
2015-02-02 16:55:11 -0800

    Added comments

--- DictSort.py
+++ DictSort.py
@@ -1,8 +1,20 @@
+##################################################################################
+#                                                                                #
+#    Sorts a dictionary based on the lexi-value of each value stored in keys.    #
+#                                                                                #
+#    AUTHOR: Nathan Brown                                                        #
+#    VERSION: 2/2/2015                                                           #
+#                                                                                #
+##################################################################################
+
 from collections import namedtuple;
 
 item = namedtuple('item', 'key value')
 
 def sortedList(dict):
+    """Sorts a dictionary into a list of named tuples
+       :param dict: dictionary to sort
+       :return: list of namedtuples"""
     sortedList = []
     for key in dict:
         sortedList.append(item(key, dict[key]))
